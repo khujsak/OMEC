1c171760c2c2a3c8dd3cff9b725a27e08e3819f0
Karl Hujsak <khujsak@gmail.com>
2018-03-18 15:59:16 -0500

    Add files via upload

---
+++ Align.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import cv2
+import numpy as np
+import os
+from utils import Stitcher
+
+imageB=cv2.imread("1.bmp")
+stitcher = Stitcher()
+storage=np.zeros([442,512,12600])
+storage[:,:,0]=cv2.cvtColor(imageB,cv2.COLOR_BGR2GRAY)
+for i in range(12600):
+    
+    fileref=str(i+2)+".bmp"    
+    imageA=cv2.imread(fileref)
+     
+    result = stitcher.stitch([imageA, imageB], showMatches=True)
+    storage[:,:,i+1]=cv2.cvtColor(result,cv2.COLOR_BGR2GRAY)
+
+
+
+os.chdir(os.getcwd() +'\\AlignedSIFT')
+np.save("Aligned.npy", storage)
+for i in range(14040):
+    im=storage[:,:,i]
+    cv2.imwrite(str(i)+'.bmp',im)
+    
+
+
+
+
+
---
+++ MakeBand.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.interpolate import interp1d
+from scipy.ndimage import map_coordinates
+import cv2
+
+
+segment=cv2.cvtColor(cv2.imread('Segment.bmp'), cv2.COLOR_BGR2GRAY)
+
+def stereographic_proj(phi, theta):
+    
+    R=np.sin(phi)/(1-np.cos(phi)+0.000000001)
+    Th=theta
+    R=(R-1)/np.max(R)
+    
+    return R, Th
+    
+    
+def orthographic_proj(phi, theta):
+    
+    R=np.cos(phi)
+    Th=theta    
+    R=R/np.max(R)
+    
+    return R, Th  
+    
+    
+def polar2cartesian(r, t, grid, x, y, order=0):
+
+    X, Y = np.meshgrid(x, y)
+
+    new_r = np.sqrt(X*X+Y*Y)
+    new_t = np.arctan2(Y,X)
+
+    ir = interp1d(r, np.arange(len(r)), bounds_error=False)
+    it = interp1d(t, np.arange(len(t)))
+
+    new_ir = ir(new_r.ravel())
+    new_it = it(new_t.ravel())
+
+    return map_coordinates(grid, np.array([new_ir, new_it]),
+                            order=order).reshape(new_r.shape)
+
+ 
+#For number of clusters in the segmented image
+for i in range(34):
+    
+    band=np.zeros([70, 180])    
+    index=0
+    segment_index=i
+    for out in range(180):
+        
+        for index in range(70):
+            index2=index+out*70
+            band[index, out]=np.average(np.extract(segment==segment_index, storage[:,:,index2]))
+    
+    nr = 70
+    nt = 180
+    
+    
+    
+    phi = np.linspace(np.pi/2,np.pi/2 - 34.5*(np.pi/180), 70)
+
+    th = np.linspace(-np.pi, np.pi, nt)
+    z = band
+    
+    # Define new cartesian grid
+    
+    nx = 256
+    ny = 256
+    
+    x = np.linspace(-1., 1., nx)
+    y = np.linspace(-1., 1., ny)
+    
+    
+    r_p, th_p=stereographic_proj(phi, th)
+    #r_p, th_p=orthographic_proj(phi, th)
+    
+    
+    band_out=polar2cartesian(r_p, th_p, z, x, y, order=3)
+    band_out[np.where(band_out==0)]=np.mean(z)
+    
+    fig = plt.figure()
+    plt.title('Segment Index = %i' %segment_index)
+    
+    ax = fig.add_subplot(121)
+    fig.subplots_adjust(left=0.25, bottom=0.25)    
+    im1=ax.imshow(band_out, interpolation='nearest', cmap='Greys_r')
+    ax.axis('off')
+    
+    ax2 = fig.add_subplot(122)  
+    im2=ax2.imshow(segment==segment_index, cmap='Greys_r')
+    ax2.axis('off')
+    
+    filename = '/home/tomosys/Documents/ECP/5-8-17 out 2/Band/'+str(i)+'band.png'
+    fig.savefig(filename, bbox_inches='none') 
+    plt.close(fig)
+    
+    filename = '/home/tomosys/Documents/ECP/5-8-17 out 2/Band/'+str(i)+'band.npy'  
+    np.save(filename, band_out)
