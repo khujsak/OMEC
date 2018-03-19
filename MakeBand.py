6be026131bfe6401b79f04360fdbc59632f8da71
Karl Hujsak <khujsak@gmail.com>
2018-03-19 16:37:18 -0500

    Update MakeBand.py

--- MakeBand.py
+++ MakeBand.py
@@ -7,8 +7,6 @@ from scipy.ndimage import map_coordinates
 import cv2
 
 
-segment=cv2.cvtColor(cv2.imread('Segment.bmp'), cv2.COLOR_BGR2GRAY)
-
 def stereographic_proj(phi, theta):
     
     R=np.sin(phi)/(1-np.cos(phi)+0.000000001)
@@ -43,60 +41,66 @@ def polar2cartesian(r, t, grid, x, y, order=0):
     return map_coordinates(grid, np.array([new_ir, new_it]),
                             order=order).reshape(new_r.shape)
 
- 
-#For number of clusters in the segmented image
-for i in range(34):
-    
-    band=np.zeros([70, 180])    
-    index=0
-    segment_index=i
-    for out in range(180):
-        
-        for index in range(70):
-            index2=index+out*70
-            band[index, out]=np.average(np.extract(segment==segment_index, storage[:,:,index2]))
-    
-    nr = 70
-    nt = 180
-    
-    
-    
-    phi = np.linspace(np.pi/2,np.pi/2 - 34.5*(np.pi/180), 70)
 
-    th = np.linspace(-np.pi, np.pi, nt)
-    z = band
+if __name__ == "__main__":
+        
+    segment=cv2.cvtColor(cv2.imread('Segment.bmp'), cv2.COLOR_BGR2GRAY)
+
     
-    # Define new cartesian grid
+     
+    #For number of clusters in the segmented image
+    for i in range(34):
+        
+        band=np.zeros([70, 180])    
+        index=0
+        segment_index=i
+        for out in range(180):
+            
+            for index in range(70):
+                index2=index+out*70
+                band[index, out]=np.average(np.extract(segment==segment_index, storage[:,:,index2]))
+        
+        nr = 70
+        nt = 180
+        
+        
+        
+        phi = np.linspace(np.pi/2,np.pi/2 - 34.5*(np.pi/180), 70)
     
-    nx = 256
-    ny = 256
-    
-    x = np.linspace(-1., 1., nx)
-    y = np.linspace(-1., 1., ny)
-    
-    
-    r_p, th_p=stereographic_proj(phi, th)
-    #r_p, th_p=orthographic_proj(phi, th)
-    
-    
-    band_out=polar2cartesian(r_p, th_p, z, x, y, order=3)
-    band_out[np.where(band_out==0)]=np.mean(z)
-    
-    fig = plt.figure()
-    plt.title('Segment Index = %i' %segment_index)
-    
-    ax = fig.add_subplot(121)
-    fig.subplots_adjust(left=0.25, bottom=0.25)    
-    im1=ax.imshow(band_out, interpolation='nearest', cmap='Greys_r')
-    ax.axis('off')
-    
-    ax2 = fig.add_subplot(122)  
-    im2=ax2.imshow(segment==segment_index, cmap='Greys_r')
-    ax2.axis('off')
-    
-    filename = '/home/tomosys/Documents/ECP/5-8-17 out 2/Band/'+str(i)+'band.png'
-    fig.savefig(filename, bbox_inches='none') 
-    plt.close(fig)
-    
-    filename = '/home/tomosys/Documents/ECP/5-8-17 out 2/Band/'+str(i)+'band.npy'  
-    np.save(filename, band_out)
+        th = np.linspace(-np.pi, np.pi, nt)
+        z = band
+        
+        # Define new cartesian grid
+        
+        nx = 256
+        ny = 256
+        
+        x = np.linspace(-1., 1., nx)
+        y = np.linspace(-1., 1., ny)
+        
+        
+        r_p, th_p=stereographic_proj(phi, th)
+        #r_p, th_p=orthographic_proj(phi, th)
+        
+        
+        band_out=polar2cartesian(r_p, th_p, z, x, y, order=3)
+        band_out[np.where(band_out==0)]=np.mean(z)
+        
+        fig = plt.figure()
+        plt.title('Segment Index = %i' %segment_index)
+        
+        ax = fig.add_subplot(121)
+        fig.subplots_adjust(left=0.25, bottom=0.25)    
+        im1=ax.imshow(band_out, interpolation='nearest', cmap='Greys_r')
+        ax.axis('off')
+        
+        ax2 = fig.add_subplot(122)  
+        im2=ax2.imshow(segment==segment_index, cmap='Greys_r')
+        ax2.axis('off')
+        
+        filename = '/home/tomosys/Documents/ECP/5-8-17 out 2/Band/'+str(i)+'band.png'
+        fig.savefig(filename, bbox_inches='none') 
+        plt.close(fig)
+        
+        filename = '/home/tomosys/Documents/ECP/5-8-17 out 2/Band/'+str(i)+'band.npy'  
+        np.save(filename, band_out)
