38d6f93a18839551b6585f91e7c51cd6b3f0ea31
Karl Hujsak <khujsak@gmail.com>
2018-03-19 16:36:04 -0500

    Update Align.py

--- Align.py
+++ Align.py
@@ -4,25 +4,26 @@ import numpy as np
 import os
 from utils import Stitcher
 
-imageB=cv2.imread("1.bmp")
-stitcher = Stitcher()
-storage=np.zeros([442,512,12600])
-storage[:,:,0]=cv2.cvtColor(imageB,cv2.COLOR_BGR2GRAY)
-for i in range(12600):
+if __name__ == "__main__":
+    imageB=cv2.imread("1.bmp")
+    stitcher = Stitcher()
+    storage=np.zeros([442,512,12600])
+    storage[:,:,0]=cv2.cvtColor(imageB,cv2.COLOR_BGR2GRAY)
+    for i in range(12600):
     
-    fileref=str(i+2)+".bmp"    
-    imageA=cv2.imread(fileref)
+        fileref=str(i+2)+".bmp"    
+        imageA=cv2.imread(fileref)
      
-    result = stitcher.stitch([imageA, imageB], showMatches=True)
-    storage[:,:,i+1]=cv2.cvtColor(result,cv2.COLOR_BGR2GRAY)
+        result = stitcher.stitch([imageA, imageB], showMatches=True)
+        storage[:,:,i+1]=cv2.cvtColor(result,cv2.COLOR_BGR2GRAY)
 
 
 
-os.chdir(os.getcwd() +'\\AlignedSIFT')
-np.save("Aligned.npy", storage)
-for i in range(14040):
-    im=storage[:,:,i]
-    cv2.imwrite(str(i)+'.bmp',im)
+    os.chdir(os.getcwd() +'\\AlignedSIFT')
+    np.save("Aligned.npy", storage)
+    for i in range(14040):
+        im=storage[:,:,i]
+        cv2.imwrite(str(i)+'.bmp',im)
     
 
 
